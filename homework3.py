48997f9bb3fe3d6629d6458839653594f8eb9159
Million Aboye <111463664+Mig1a@users.noreply.github.com>
2023-07-25 17:07:23 -0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,6 @@
+#Write a for loop that iterates through numbers from 1 to 10 using the range function,
+# adds them up together, and prints the total.The printed output should be 55.
+x = 0
+for n in range(1, 11):
+    x = x + n
+    print(x)
