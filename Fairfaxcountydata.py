d4a8fbf12f4ff968f32e10e7061b2ddf8bd7c161
Million Aboye <111463664+Mig1a@users.noreply.github.com>
2023-12-24 02:00:31 -0500

    Update Fairfaxcountydata.py

--- Fairfaxcountydata.py
+++ Fairfaxcountydata.py
@@ -1,6 +1,6 @@
 import pandas as pd
 ## Loading the data -----------
-df = pd.read_csv("C:/Users/mella/OneDrive/Desktop/GMU/IT 209/CrimeReports (1).csv")
+df = pd.read_csv("CrimeReports (1).csv")
 headers = ["A", "Code", "Crime_Type", "Date", "Time", "Address", "City", "abb", "Zipcode"]
 df.columns = headers
 
@@ -9,7 +9,7 @@ df.columns = headers
 count = df.groupby(['City', "Crime_Type"])["Crime_Type"].value_counts()
 most_reported_crime = count.groupby(level=0).idxmax().apply(lambda x: x[1])
 new_count = pd.DataFrame(most_reported_crime)
-new_count.to_excel("C:/Users/mella/OneDrive/Desktop/GMU/IT 209/Most_reported_Crimetype_in_city.xlsx",
+new_count.to_excel("Most_reported_Crimetype_in_city.xlsx",
                        index=True)
 
 ## crime count by city -----------
@@ -20,7 +20,7 @@ count = df[['City', "Crime_Type"]].groupby("City").count().sort_values("Crime_Ty
 
 ## Freqently occured incident---------
 freq = df.groupby("Crime_Type")["Crime_Type"].count().sort_values(ascending=False)
-freq.to_excel("C:/Users/mella/OneDrive/Desktop/GMU/IT 209/Freq_incident.xlsx",
+freq.to_excel("Freq_incident.xlsx",
                   index=True)
 
 
@@ -38,4 +38,4 @@ def categorize_time(hour):
 df['Hour'] = df['Time'] // 100
 df['Time_Category'] = df['Hour'].apply(Report.categorize_time())
 trend = df.groupby(["Time_Category", "Crime_Type"])["Crime_Type"].count().sort_values("Crime_Type",ascending=False)
-trend.to_excel("C:/Users/mella/OneDrive/Desktop/GMU/IT 209/time_trend.xlsx",index=True)
+trend.to_excel("time_trend.xlsx",index=True)
